8cee17fdb232321aa78a3c7ab445f963fea5836d
plasmaDestroyer <satyasheel2004@gmail.com>
2026-03-31 05:41:24 +0530

    feat: sanitize icon name by *slugify*-ing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::path::{Path, PathBuf};
 use std::error::Error;
+use std::path::{Path, PathBuf};
 
 const DEFAULT_ICON: &[u8] = include_bytes!("../assets/default.png");
 
@@ -17,36 +17,48 @@ fn fetch_favicon(url: &str) -> Option<Vec<u8>> {
 
 fn get_share_dir() -> Result<PathBuf, Box<dyn Error>> {
     if let Ok(home_directory) = std::env::var("XDG_DATA_HOME") {
-        return Ok(PathBuf::from(home_directory));
+        Ok(PathBuf::from(home_directory))
     } else if let Ok(home_directory) = std::env::var("HOME") {
-        return Ok(PathBuf::from(home_directory).join(".local/share/"));
+        Ok(PathBuf::from(home_directory).join(".local/share/"))
     } else {
-        return Err("Could not find home directory!".into());
+        Err("Could not find home directory!".into())
     }
 }
 
-fn save_icon(name: &str, bytes: &[u8], share_dir: &Path) {
+fn slugify(name: &str) -> String {
+    name.to_ascii_lowercase()
+        .chars()
+        .map(|c| if c.is_ascii_alphanumeric() { c } else { '-' })
+        .collect::<String>()
+        .split('-')
+        .filter(|x| !x.is_empty())
+        .collect::<Vec<_>>()
+        .join("-")
+}
+
+fn save_icon(slug: &str, bytes: &[u8], share_dir: &Path) {
     let icons_dir = share_dir.join("icons");
     std::fs::create_dir_all(&icons_dir)
         .unwrap_or_else(|_| panic!("Error making directory: {}!", icons_dir.display()));
 
-    let icon_path = icons_dir.join(format!("{}.png", name));
+    let icon_path = icons_dir.join(format!("{}.png", slug));
     std::fs::write(icon_path, bytes).expect("Error writing image bytes to file!");
 }
 
-fn create_desktop_file(name: &str, url: &str, share_dir: &Path) {
+fn create_desktop_file(name: &str, slug: &str, url: &str, share_dir: &Path) {
     let applications_dir = share_dir.join("applications");
     std::fs::create_dir_all(&applications_dir)
         .unwrap_or_else(|_| panic!("Error making directory: {}!", applications_dir.display()));
 
-    let icon_path = share_dir.join("icons").join(format!("{}.png", name));
-    let desktop_file_path = applications_dir.join(format!("{}.desktop", name));
+    let icon_path = share_dir.join("icons").join(format!("{}.png", slug));
+    let desktop_file_path = applications_dir.join(format!("{}.desktop", slug));
     let contents = format!(
         "[Desktop Entry]
 Name={}
 Exec=chromium --app={}
 Icon={}
 Type=Application
+Terminal=false
 Categories=Network;",
         name,
         url,
@@ -69,18 +81,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Installing {} from {}", name, url);
 
     let share_dir = get_share_dir()?;
+    let slug = slugify(name);
 
     println!("Fetching favicon for {}...", url);
     if let Some(bytes) = fetch_favicon(url) {
         println!("Favicon fetched successfully!");
-        save_icon(name, &bytes, &share_dir);
+        save_icon(&slug, &bytes, &share_dir);
         println!("Icon saved.")
     } else {
         println!("Favicon not found :( ... Installing with Default icon.");
-        save_icon(name, DEFAULT_ICON, &share_dir);
+        save_icon(&slug, DEFAULT_ICON, &share_dir);
     }
 
-    create_desktop_file(name, url, &share_dir);
+    create_desktop_file(name, &slug, url, &share_dir);
     println!("Desktop file created.");
 
     println!("✓ {} installed successfully!", name);
